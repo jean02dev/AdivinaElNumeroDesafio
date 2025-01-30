52d82f8ca17646793894d3cbc045640c4ff9f701
jean02dev <jcmr0212@gmail.com>
2025-01-29 21:48:37 -0400

    se siguio maquetando el widget para mostrar las listas. Falta separar en un componente aparte

--- lib/presentation/screens/game_screen.dart
+++ lib/presentation/screens/game_screen.dart
@@ -14,57 +14,116 @@ class GameScreen extends ConsumerWidget {
     final levels = Level.values;
     final TextEditingController numberController = TextEditingController();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Juego de Adivinanza'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Center(child: Text('Número: ${gameProvider.targetNumber}')),
-            Center(child: Text('Intentos: ${gameProvider.attempts}')),
-            SizedBox(height: 20),
-            Text('Nivel Actual', textAlign: TextAlign.center),
-            SizedBox(height: 20),
-            Slider(
-              value: levels.indexOf(currentLevel).toDouble(),
-              min: 0,
-              max: levels.length - 1.toDouble(),
-              divisions: levels.length - 1,
-              label: currentLevel.toString().split('.').last,
-              onChanged: (value) {
-                final newLevel = levels[value.toInt()];
-                ref.read(gameProvidersProvider.notifier).updateLevel(newLevel);
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Center(child: Text('Número: ${gameProvider.targetNumber}')),
+          Center(child: Text('Intentos: ${gameProvider.attempts}')),
+          SizedBox(height: 20),
+          Text('Nivel Actual', textAlign: TextAlign.center),
+          SizedBox(height: 20),
+          Slider(
+            value: levels.indexOf(currentLevel).toDouble(),
+            min: 0,
+            max: levels.length - 1.toDouble(),
+            divisions: levels.length - 1,
+            label: currentLevel.toString().split('.').last,
+            onChanged: (value) {
+              final newLevel = levels[value.toInt()];
+              ref.read(gameProvidersProvider.notifier).updateLevel(newLevel);
+            },
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              controller: numberController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Adivina un número',
+                border: OutlineInputBorder(),
+              ),
+              onSubmitted: (value) {
+                int guessedNumber = int.tryParse(value) ?? 0;
+                ref
+                    .read(gameProvidersProvider.notifier)
+                    .guessNumber(guessedNumber);
               },
             ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: TextField(
-                controller: numberController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Adivina un número',
-                  border: OutlineInputBorder(),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text('Mayor que'),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: gameProvider.lowerNumbers.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              dense: true,
+                              title: Center(
+                                child: Text(gameProvider.lowerNumbers[index]
+                                    .toString()),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                onSubmitted: (value) {
-                  int guessedNumber = int.tryParse(value) ?? 0;
-                  ref
-                      .read(gameProvidersProvider.notifier)
-                      .guessNumber(guessedNumber);
-                },
-              ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text('Menor que'),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: gameProvider.higherNumbers.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              dense: true,
+                              title: Center(
+                                child: Text(gameProvider.higherNumbers[index]
+                                    .toString()),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text('Historial'),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: gameProvider.history.length,
+                          itemBuilder: (context, index) {
+                            return Center(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 2),
+                                child: Text(
+                                  gameProvider.history[index].toString(),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            if (gameProvider.lowerNumbers.isNotEmpty)
-              Text('Mayor que: ${gameProvider.lowerNumbers.join(', ')}'),
-            if (gameProvider.higherNumbers.isNotEmpty)
-              Text('Menor que: ${gameProvider.higherNumbers.join(', ')}'),
-            if (gameProvider.history.isNotEmpty)
-              Text('History: ${gameProvider.history.join(', ')}'),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
